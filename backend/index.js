32c34f1dc26aa394180d10ae8e15ced9f6d2f1b7
SetupWeb <saifahmad786sm@gmail.com>
2024-12-03 01:02:48 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://opportunity-hub-alpha.vercel.app/",
     credentials:true
 }
 app.use(cors(corsOptions));
