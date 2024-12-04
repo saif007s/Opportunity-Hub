67d1737b49c378197f72649015e4fa832632eb62
SetupWeb <saifahmad786sm@gmail.com>
2024-12-04 22:45:01 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config();
 // connect db
@@ -16,13 +17,16 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 
+const _dirname = path.resolve();
+
+
 // middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://opportunity-hub-alpha.vercel.app/",
+    origin:"http://localhost:8000",
     credentials:true
 }
 app.use(cors(corsOptions));
@@ -32,6 +36,12 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+
+});
 
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}`);
